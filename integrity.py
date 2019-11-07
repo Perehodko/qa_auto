db37c8ec0563ef6ede678f11c499c844000b03ca
Perehodko <n.perehodko@yandex.com>
2019-11-07 11:33:38 +0300

    refactoring

--- integrity.py
+++ integrity.py
@@ -1,38 +1,30 @@
 from hashlib import md5, sha1, sha256
 import sys
 
-path_to_file = sys.argv[1]
-path_to_dir = sys.argv[2]
+try:
+    path_to_file, path_to_dir = sys.argv[1], sys.argv[2]
+except IndexError as e:
+    print("Arguments: {}!".format(e))
+    raise SystemExit
 
 with open(path_to_file) as f:
     for line in f:
         line = line.rstrip()
         file_name, algorithm, etalon_hash = line.split()
 
-        if algorithm == "md5":
-            try:
-                real_hash = md5(open(path_to_dir + file_name, "rb").read().rstrip()).hexdigest()
-                if etalon_hash == real_hash:
-                    print(file_name, "OK")
-                else:
-                    print(file_name, "FAIL")
-            except FileNotFoundError:
-                print(file_name, "NOT FOUND")
-        elif algorithm == "sha1":
-            try:
-                real_hash = sha1(open(path_to_dir + file_name, "rb").read().rstrip()).hexdigest()
-                if etalon_hash == real_hash:
-                    print(file_name, "OK")
-                else:
-                    print(file_name, "FAIL")
-            except FileNotFoundError:
-                print(file_name, "NOT FOUND")
-        elif algorithm == "sha256":
-            try:
-                real_hash = sha256(open(path_to_dir + file_name, "rb").read().rstrip()).hexdigest()
-                if etalon_hash == real_hash:
-                    print(file_name, "OK")
-                else:
-                    print(file_name, "FAIL")
-            except FileNotFoundError:
-                print(file_name, "NOT FOUND")
+        try:
+            text = open(path_to_dir + file_name, "rb").read().rstrip()
+            if algorithm == "md5":
+                real_hash = md5(text).hexdigest()
+            elif algorithm == "sha1":
+                real_hash = sha1(text).hexdigest()
+            elif algorithm == "sha256":
+                real_hash = sha256(text).hexdigest()
+
+            if etalon_hash == real_hash:
+                print(file_name, "OK")
+            else:
+                print(file_name, "FAIL")
+
+        except FileNotFoundError:
+            print(file_name, "NOT FOUND")
